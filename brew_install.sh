09586318cc3a3ef7bdd5893aea2e89846900d9d7
foursue <foursue@gmail.com>
2017-08-21 13:06:29 +0900

    first commit

---
+++ brew_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 現在入ってるパッケージ一覧は`brew ls`で列挙できる
+brew install autoconf
+brew install composer
+brew install freetype
+brew install gdbm
+brew install go
+brew install libpng
+brew install matplotlib
+brew install mysql
+brew install nkf
+brew install numpy
+brew install openssl
+brew install pkg-config
+brew install putty
+brew install pyenv
+brew install pyenv-virtualenv
+brew install python
+brew install python3
+brew install rbenv
+brew install readline
+brew install ruby-build
+brew install screen
+brew install sip
+brew install smlnj
+brew install sqlite
+brew install tree
+brew install wget
+brew install xz
